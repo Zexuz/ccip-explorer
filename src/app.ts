8fa590aff4a5db1a567ef1ca608b57d4c32242a5
Zexuz <robin.edbom@gmail.com>
2023-08-15 15:18:39 +0100

    Add API for transaction details

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import {getAbiFromMongoDb, getEventsFromMongoDb, saveAbiToMongoDb} from "./mongodb";
+import {getAbiFromMongoDb, getEventsFromMongoDb, getEventWithMessageIdFromMongoDb, saveAbiToMongoDb} from "./mongodb";
+import express from 'express'
 
-require('dotenv').config()
+require('dotenv')
+  .config()
 
 import {createProvider, getProviderForNetwork, ILane, NetworkNames} from "./observers/infura";
 import {Explorer, ExplorerFactory} from "./explorers/explorer";
@@ -12,240 +14,262 @@ import * as process from "process";
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 class FileService {
-    constructor() {
+  constructor() {
+  }
+
+  public async saveAbi(network: NetworkNames, address: string, contractName: string, abi: any): Promise<void> {
+    await saveToFile(this.getAbiPath(network, address, contractName), abi);
+  }
+
+  public async getAbi(network: NetworkNames, address: string, contractName: string): Promise<any> {
+    return await readFromFile(this.getAbiPath(network, address, contractName));
+  }
+
+  public async getLanesForNetwork(network: NetworkNames): Promise<ILane[]> {
+    const dirs = listDirs(`/addresses/${network}`);
+    const onRamps = dirs.filter(dir => dir.startsWith("onRamp-"));
+    const createLane = async (dir: string) => {
+      const source = network;
+      const destination = dir.split("-")[1];
+      const address = dir.split("-")[2];
+      const abi = await this.getAbi(source, address, `onRamp-${destination}`);
+      return {
+        source,
+        destination,
+        address,
+        abi,
+        events: ['CCIPSendRequested']
+      } as ILane;
     }
 
-    public async saveAbi(network: NetworkNames, address: string, contractName: string, abi: any): Promise<void> {
-        await saveToFile(this.getAbiPath(network, address, contractName), abi);
-    }
+    return await Promise.all(onRamps.map(createLane));
+  }
 
-    public async getAbi(network: NetworkNames, address: string, contractName: string): Promise<any> {
-        return await readFromFile(this.getAbiPath(network, address, contractName));
-    }
-
-    public async getLanesForNetwork(network: NetworkNames): Promise<ILane[]> {
-        const dirs = listDirs(`/addresses/${network}`);
-        const onRamps = dirs.filter(dir => dir.startsWith("onRamp-"));
-        const createLane = async (dir: string) => {
-            const source = network;
-            const destination = dir.split("-")[1];
-            const address = dir.split("-")[2];
-            const abi = await this.getAbi(source, address, `onRamp-${destination}`);
-            return {
-                source,
-                destination,
-                address,
-                abi,
-                events: ['CCIPSendRequested']
-            } as ILane;
-        }
-
-        return await Promise.all(onRamps.map(createLane));
-    }
-
-    private getAbiPath(network: NetworkNames, address: string, contractName: string): string {
-        return `/addresses/${network}/${contractName}-${address}/abi.json`;
-    }
+  private getAbiPath(network: NetworkNames, address: string, contractName: string): string {
+    return `/addresses/${network}/${contractName}-${address}/abi.json`;
+  }
 }
 
 const getAbi = async (address: string, explorer: Explorer) => {
-    const abi = await explorer.getContractAbi(address);
-    return JSON.stringify(abi);
+  const abi = await explorer.getContractAbi(address);
+  return JSON.stringify(abi);
 }
 
 interface Config {
-    address: string;
-    chainSelector: string;
-    isTestNet: boolean;
-    network: NetworkNames,
-    isEnable: boolean;
+  address: string;
+  chainSelector: string;
+  isTestNet: boolean;
+  network: NetworkNames,
+  isEnable: boolean;
 }
 
 async function init(configs: Config[]) {
-    const fileService = new FileService();
+  const fileService = new FileService();
 
-    for (const {network, address, isTestNet} of configs) {
-        const explorer = new ExplorerFactory().getExplorer(network);
-        const abi = await getAbi(address, explorer);
-        await fileService.saveAbi(network, address, "router", abi)
+  for (const {network, address, isTestNet} of configs) {
+    const explorer = new ExplorerFactory().getExplorer(network);
+    const abi = await getAbi(address, explorer);
+    await fileService.saveAbi(network, address, "router", abi)
 
-        const {jsonRpcProver} = getProviderForNetwork(network)
-        const contract = new ethers.Contract(address, abi, jsonRpcProver);
+    const {jsonRpcProver} = getProviderForNetwork(network)
+    const contract = new ethers.Contract(address, abi, jsonRpcProver);
 
-        const chainSelectors = configs
-            .filter(config => config.network !== network)
-            .filter(config => config.isTestNet === isTestNet)
-            .map(url => url.chainSelector);
+    const chainSelectors = configs
+      .filter(config => config.network !== network)
+      .filter(config => config.isTestNet === isTestNet)
+      .map(url => url.chainSelector);
 
-        for (const chainSelector of chainSelectors) {
-            const destNetwork = configs.find(config => config.chainSelector === chainSelector)?.network;
-            try {
-                const result = await contract.getOnRamp(chainSelector) as string;
-                if (result.startsWith("0x0000000000000000")) {
-                    console.log(`Lane ${network} -> ${destNetwork} is not configured`)
-                    continue;
-                }
-                await sleep(1000);
-                const abi = await getAbi(result, explorer);
-                await fileService.saveAbi(network, result, `onRamp-${destNetwork}`, abi)
-                console.log(`Lane ${network} -> ${destNetwork}, address: ${result}`)
-            } catch (error) {
-                console.error("Error calling contract method:", error);
-            }
+    for (const chainSelector of chainSelectors) {
+      const destNetwork = configs.find(config => config.chainSelector === chainSelector)?.network;
+      try {
+        const result = await contract.getOnRamp(chainSelector) as string;
+        if (result.startsWith("0x0000000000000000")) {
+          console.log(`Lane ${network} -> ${destNetwork} is not configured`)
+          continue;
         }
+        await sleep(1000);
+        const abi = await getAbi(result, explorer);
+        await fileService.saveAbi(network, result, `onRamp-${destNetwork}`, abi)
+        console.log(`Lane ${network} -> ${destNetwork}, address: ${result}`)
+      } catch (error) {
+        console.error("Error calling contract method:", error);
+      }
     }
+  }
 }
 
 interface CCIPMessage {
-    source: string;
-    destination: string;
-    isTestNet: boolean;
-    senderAddress: string;
-    receiverAddress: string;
-    data: string;
+  source: string;
+  destination: string;
+  isTestNet: boolean;
+  senderAddress: string;
+  receiverAddress: string;
+  data: string;
 }
 
 
 (async () => {
 
 
-    const configs: Config[] = [
-        {
-            network: NetworkNames.ETH_SEPOLIA,
-            address: "0xD0daae2231E9CB96b94C8512223533293C3693Bf",
-            chainSelector: "16015286601757825753",
-            isTestNet: true,
-            isEnable: true,
-        },
-        {
-            network: NetworkNames.ETH_MAINNET,
-            address: "0xE561d5E02207fb5eB32cca20a699E0d8919a1476",
-            chainSelector: "5009297550715157269",
-            isTestNet: false,
-            isEnable: true,
-        },
-        {
-            network: NetworkNames.OPTIMISM_MAINNET,
-            address: "0x261c05167db67B2b619f9d312e0753f3721ad6E8",
-            chainSelector: "3734403246176062136",
-            isTestNet: false,
-            isEnable: true,
-        },
-        {
-            network: NetworkNames.OPTIMISM_GOERLI,
-            address: "0xEB52E9Ae4A9Fb37172978642d4C141ef53876f26",
-            chainSelector: "2664363617261496610",
-            isTestNet: true,
-            isEnable: true,
+  const configs: Config[] = [
+    {
+      network: NetworkNames.ETH_SEPOLIA,
+      address: "0xD0daae2231E9CB96b94C8512223533293C3693Bf",
+      chainSelector: "16015286601757825753",
+      isTestNet: true,
+      isEnable: true,
+    },
+    {
+      network: NetworkNames.ETH_MAINNET,
+      address: "0xE561d5E02207fb5eB32cca20a699E0d8919a1476",
+      chainSelector: "5009297550715157269",
+      isTestNet: false,
+      isEnable: true,
+    },
+    {
+      network: NetworkNames.OPTIMISM_MAINNET,
+      address: "0x261c05167db67B2b619f9d312e0753f3721ad6E8",
+      chainSelector: "3734403246176062136",
+      isTestNet: false,
+      isEnable: true,
+    },
+    {
+      network: NetworkNames.OPTIMISM_GOERLI,
+      address: "0xEB52E9Ae4A9Fb37172978642d4C141ef53876f26",
+      chainSelector: "2664363617261496610",
+      isTestNet: true,
+      isEnable: true,
 
-        },
-        {
-            network: NetworkNames.ARBITRUM_GOERLI,
-            address: "0x88E492127709447A5ABEFdaB8788a15B4567589E",
-            chainSelector: "6101244977088475029",
-            isTestNet: true,
-            isEnable: true,
-        },
-        {
-            network: NetworkNames.POLYGON_MAINNET,
-            address: "0x3C3D92629A02a8D95D5CB9650fe49C3544f69B43",
-            chainSelector: "4051577828743386545",
-            isTestNet: false,
-            isEnable: true,
+    },
+    {
+      network: NetworkNames.ARBITRUM_GOERLI,
+      address: "0x88E492127709447A5ABEFdaB8788a15B4567589E",
+      chainSelector: "6101244977088475029",
+      isTestNet: true,
+      isEnable: true,
+    },
+    {
+      network: NetworkNames.POLYGON_MAINNET,
+      address: "0x3C3D92629A02a8D95D5CB9650fe49C3544f69B43",
+      chainSelector: "4051577828743386545",
+      isTestNet: false,
+      isEnable: true,
 
-        },
-        {
-            network: NetworkNames.POLYGON_MUMBAI,
-            address: "0x70499c328e1E2a3c41108bd3730F6670a44595D1",
-            chainSelector: "12532609583862916517",
-            isTestNet: true,
-            isEnable: true,
-        },
-        {
-            network: NetworkNames.AVAX_MAINNET,
-            address: "0x27F39D0af3303703750D4001fCc1844c6491563c",
-            chainSelector: "6433500567565415381",
-            isTestNet: false,
-            isEnable: false,
-        },
-        {
-            network: NetworkNames.AVAX_FUJI,
-            address: "0x554472a2720E5E7D5D3C817529aBA05EEd5F82D8",
-            chainSelector: "14767482510784806043",
-            isTestNet: true,
-            isEnable: true,
-        }
-    ]
-    const enableConfigs = configs.filter(value => value.isEnable);
-
-    if (process.env.INIT) {
-        await init(enableConfigs);
+    },
+    {
+      network: NetworkNames.POLYGON_MUMBAI,
+      address: "0x70499c328e1E2a3c41108bd3730F6670a44595D1",
+      chainSelector: "12532609583862916517",
+      isTestNet: true,
+      isEnable: true,
+    },
+    {
+      network: NetworkNames.AVAX_MAINNET,
+      address: "0x27F39D0af3303703750D4001fCc1844c6491563c",
+      chainSelector: "6433500567565415381",
+      isTestNet: false,
+      isEnable: false,
+    },
+    {
+      network: NetworkNames.AVAX_FUJI,
+      address: "0x554472a2720E5E7D5D3C817529aBA05EEd5F82D8",
+      chainSelector: "14767482510784806043",
+      isTestNet: true,
+      isEnable: true,
     }
+  ]
+  const enableConfigs = configs.filter(value => value.isEnable);
 
-    const events = await getEventsFromMongoDb(new Date("2021-10-01T00:00:00.000Z").getTime() / 1000);
+  if (process.env.INIT) {
+    await init(enableConfigs);
+  }
 
-    const convertEvent = (event: any) => {
-        const ccipMessage: CCIPMessage = {
-            source: event.senderChain,
-            destination: event.destinationChain,
-            isTestNet: !event.senderChain.endsWith('MAINNET'),
-            senderAddress: event.message.sender,
-            receiverAddress: event.message.receiver,
-            data: event.message.data,
-        }
-        return ccipMessage;
+  const events = await getEventsFromMongoDb(new Date("2021-10-01T00:00:00.000Z").getTime() / 1000);
+
+  const convertEvent = (event: any) => {
+    const ccipMessage: CCIPMessage = {
+      source: event.senderChain,
+      destination: event.destinationChain,
+      isTestNet: !event.senderChain.endsWith('MAINNET'),
+      senderAddress: event.message.sender,
+      receiverAddress: event.message.receiver,
+      data: event.message.data,
     }
+    return ccipMessage;
+  }
 
-    const processEvent = async (convertedEvent: CCIPMessage, type: 'sender' | 'receiver') => {
-        const explorerType = type === 'sender' ? convertedEvent.source : convertedEvent.destination;
-        const address = type === 'sender' ? convertedEvent.senderAddress : convertedEvent.receiverAddress;
-        const chainName = type === 'sender' ? 'source' : 'destination';
+  const processEvent = async (convertedEvent: CCIPMessage, type: 'sender' | 'receiver') => {
+    const explorerType = type === 'sender' ? convertedEvent.source : convertedEvent.destination;
+    const address = type === 'sender' ? convertedEvent.senderAddress : convertedEvent.receiverAddress;
+    const chainName = type === 'sender' ? 'source' : 'destination';
 
-        const explorer = new ExplorerFactory().getExplorer(explorerType as NetworkNames);
-        const contract = await getAbiFromMongoDb(explorerType, address);
-        if (!contract) {
-            let abi;
-            try {
-                abi = await explorer.getContractAbi(address);
-            } catch (e) {
-                console.log(`${type.charAt(0).toUpperCase() + type.slice(1)} address ${address} not found on ${convertedEvent[chainName]}`);
-            } finally {
-                await saveAbiToMongoDb(explorerType, address, abi, true);
-            }
-        }
-    };
-
-    const processAllEvents = async (events: any) => {
-        const ccipMessages = [];
-        for (const event of events) {
-            const convertedEvent = convertEvent(event);
-
-            await processEvent(convertedEvent, 'sender');
-            await processEvent(convertedEvent, 'receiver');
-            await sleep(300);
-
-            ccipMessages.push(convertedEvent);
-        }
-        return ccipMessages;
-    };
-
-    const ccipMessages = await processAllEvents(events);
-
-    console.log(`Total events: ${ccipMessages.length}`);
-
-
-    return
-    const fileService = new FileService();
-
-    for (const {network} of enableConfigs) {
-        const lanes = await fileService.getLanesForNetwork(network as NetworkNames);
-        console.log(`Lanes for ${network}: ${lanes.map(value => value.destination).join(", ")}`);
-        await sleep(300);
-        createProvider(network as NetworkNames, lanes);
+    const explorer = new ExplorerFactory().getExplorer(explorerType as NetworkNames);
+    const contract = await getAbiFromMongoDb(explorerType, address);
+    if (!contract) {
+      let abi;
+      try {
+        abi = await explorer.getContractAbi(address);
+      } catch (e) {
+        console.log(`${type.charAt(0)
+                           .toUpperCase() + type.slice(1)} address ${address} not found on ${convertedEvent[chainName]}`);
+      } finally {
+        await saveAbiToMongoDb(explorerType, address, abi, true);
+      }
     }
-    //22:18:00 last event on ccip.chain.link
+  };
 
-    // while (true) {
-    //     await sleep(500);
-    // }
+  const processAllEvents = async (events: any) => {
+    const ccipMessages = [];
+    for (const event of events) {
+      const convertedEvent = convertEvent(event);
+
+      await processEvent(convertedEvent, 'sender');
+      await processEvent(convertedEvent, 'receiver');
+      await sleep(300);
+
+      ccipMessages.push(convertedEvent);
+    }
+    return ccipMessages;
+  };
+
+  // const ccipMessages = await processAllEvents(events);
+  //
+  // console.log(`Total events: ${ccipMessages.length}`);
+
+  const startExpressServer = () => {
+    const app = express()
+    const port = 3001
+
+    app.use(express.json());
+
+    app.get('/api/msg/:msg', async (req: any, res: any) => {
+      const msg = req.params.msg;
+      const message = await getEventWithMessageIdFromMongoDb(msg)
+      res.json({msg: message});
+    });
+
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`)
+    })
+  }
+
+
+  startExpressServer()
+
+  return
+  const fileService = new FileService();
+
+  for (const {network} of enableConfigs) {
+    const lanes = await fileService.getLanesForNetwork(network as NetworkNames);
+    console.log(`Lanes for ${network}: ${lanes.map(value => value.destination)
+                                              .join(", ")}`);
+    await sleep(300);
+    createProvider(network as NetworkNames, lanes);
+  }
+  //22:18:00 last event on ccip.chain.link
+
+  // while (true) {
+  //     await sleep(500);
+  // }
 })();
+
--- src/mongodb.ts
+++ src/mongodb.ts
@@ -5,81 +5,123 @@ const MONGO_URI = 'mongodb://localhost:27017';  // E.g., "mongodb://localhost:27
 const client = new MongoClient(MONGO_URI);
 
 export const saveToMongoDb = async (event: any) => {
-    await client.connect();
-    console.log('Connected to MongoDB');
+  await client.connect();
+  console.log('Connected to MongoDB');
 
-    const db = client.db('ccip');
+  const db = client.db('ccip');
 
-    try {
-        const collection = db.collection('events');  // "events" is the name of the collection where we'll save our events
-        await collection.insertOne(event);
-        console.log("Event saved successfully!");
-    } catch (error) {
-        console.error("Error saving the event:", error);
-    } finally {
-        await client.close();
-    }
+  try {
+    const collection = db.collection('events');  // "events" is the name of the collection where we'll save our events
+    await collection.insertOne(event);
+    console.log("Event saved successfully!");
+  } catch (error) {
+    console.error("Error saving the event:", error);
+  } finally {
+    await client.close();
+  }
 }
 
 export const getEventsFromMongoDb = async (lastTimestamp: number) => {
-    await client.connect();
-    console.log('Connected to MongoDB');
+  await client.connect();
+  console.log('Connected to MongoDB');
 
-    const db = client.db('ccip');
+  const db = client.db('ccip');
 
-    try {
-        const collection = db.collection('events');  // "events" is the name of the collection where we'll save our events
-        const events = await collection.find({timestamp: {$gt: lastTimestamp}}).toArray();
-        console.log("Events retrieved successfully!");
-        return events;
-    } catch (error) {
-        console.error("Error retrieving the events:", error);
-        return [];
-    } finally {
-        await client.close();
-    }
+  try {
+    const collection = db.collection('events');  // "events" is the name of the collection where we'll save our events
+    const events = await collection.find({timestamp: {$gt: lastTimestamp}})
+                                   .toArray();
+    console.log("Events retrieved successfully!");
+    return events;
+  } catch (error) {
+    console.error("Error retrieving the events:", error);
+    return [];
+  } finally {
+    await client.close();
+  }
+}
+
+const databaseEvent = {
+  "_id": {"$oid": "64d58fac0b8054c1eac0264d"},
+  "destinationChain": "ARBITRUM_GOERLI",
+  "eventName": "CCIPSendRequested",
+  "message": {
+    "sourceChainSelector": "2664363617261496610",
+    "sequenceNumber": "7290",
+    "feeTokenAmount": "99790498461538461",
+    "sender": "0x2Af63f50Fa3F97f4AA94D28327A759ca86B33BF8",
+    "nonce": "2625",
+    "gasLimit": "200000",
+    "strict": false,
+    "receiver": "0x57C0059Fc3f98aA0a5CE4Fb5d2882d81D839E74F",
+    "data": "0x0000000000000000000000000000000000000000000000000000000000000282",
+    "tokenAmounts": [],
+    "feeToken": "0xdc2CC710e42857672E7907CF474a69B63B93089f",
+    "messageId": "0xc7b22b9d813993386874de616066e4336e37f0d33aea4fc77570ad8226fa7e68"
+  },
+  "senderChain": "OPTIMISM_GOERLI",
+  "timestamp": 1691717548.984
+}
+
+export const getEventWithMessageIdFromMongoDb = async (messageId: string) => {
+  await client.connect();
+  console.log('Connected to MongoDB');
+
+  const db = client.db('ccip');
+
+  try {
+    const collection = db.collection('events');  // "events" is the name of the collection where we'll save our events
+    const events = await collection.findOne({"message.messageId": messageId});
+    console.log("Events retrieved successfully!");
+    return events;
+  } catch (error) {
+    console.error("Error retrieving the events:", error);
+    return [];
+  } finally {
+    await client.close();
+  }
 }
 
 interface Contract {
-    chain: string;
-    address: string;
-    abi: string | null;
-    haveCheckedExplorer: boolean;
+  chain: string;
+  address: string;
+  abi: string | null;
+  haveCheckedExplorer: boolean;
 }
 
 
 export const getAbiFromMongoDb = async (chain: string, address: string): Promise<Contract | null> => {
-    await client.connect();
-    console.log('Connected to MongoDB');
+  await client.connect();
+  console.log('Connected to MongoDB');
 
-    const db = client.db('ccip');
+  const db = client.db('ccip');
 
-    try {
-        const collection = db.collection('abis');  // "abis" is the name of the collection where we'll save our abis
-        const abi = await collection.findOne({chain, address});
-        console.log("Abi retrieved successfully!");
-        return abi as Contract | null;
-    } catch (error) {
-        console.error("Error retrieving the abi:", error);
-        return null;
-    } finally {
-        await client.close();
-    }
+  try {
+    const collection = db.collection('abis');  // "abis" is the name of the collection where we'll save our abis
+    const abi = await collection.findOne({chain, address});
+    console.log("Abi retrieved successfully!");
+    return abi as Contract | null;
+  } catch (error) {
+    console.error("Error retrieving the abi:", error);
+    return null;
+  } finally {
+    await client.close();
+  }
 }
 
 export const saveAbiToMongoDb = async (chain: string, address: string, abi: string | null, haveCheckedExplorer: boolean) => {
-    await client.connect();
-    console.log('Connected to MongoDB');
+  await client.connect();
+  console.log('Connected to MongoDB');
 
-    const db = client.db('ccip');
+  const db = client.db('ccip');
 
-    try {
-        const collection = db.collection('abis');  // "abis" is the name of the collection where we'll save our abis
-        await collection.insertOne({chain, address, abi, haveCheckedExplorer});
-        console.log("Abi saved successfully!");
-    } catch (error) {
-        console.error("Error saving the abi:", error);
-    } finally {
-        await client.close();
-    }
+  try {
+    const collection = db.collection('abis');  // "abis" is the name of the collection where we'll save our abis
+    await collection.insertOne({chain, address, abi, haveCheckedExplorer});
+    console.log("Abi saved successfully!");
+  } catch (error) {
+    console.error("Error saving the abi:", error);
+  } finally {
+    await client.close();
+  }
 }
